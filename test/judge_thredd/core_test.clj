fef482400a59cb44c8d1021df9e1b6521cc5cbcd
Daemian Mack <daemianmack@gmail.com>
2016-01-11 08:48:06 -0500

    Bring dummy test in line with README.

--- test/judge_thredd/core_test.clj
+++ test/judge_thredd/core_test.clj
@@ -3,25 +3,14 @@
             [clojure.test :refer :all]))
 
 (def test-conversation
-  [[0 "alice"   "good morning"]
-   [1  "bill"   "alice: good morning"]
-   [5  "chuck"  "anybody up for breakfast burritos?"]
-   [7  "daisy"  "chuck: i loathe oxygen!"]
-   [90 "edward" "alice: morning!"]])
+  [[0 "alice"  "good morning"]
+   [1  "bill"  "alice: good morning"]
+   [5  "chuck" "anybody up for breakfast burritos?"]
+   [7  "daisy" "chuck: i love breakfast burritos!"]
+   [90 "eddie" "alice: morning!"]])
 
-(def test-conversation-result
-  {"alice" [{:members #{"alice" "bill" "edward"}
-             :start 0
-             :stop 90
-             :msgs [[0   "alice" "good morning"]
-                    [1    "bill" "alice: good morning"]
-                    [90 "edward" "alice: morning!"]]}]
-   "chuck" [{:members #{"chuck" "daisy"}
-             :start 5
-             :stop  7
-             :msgs [[5 "chuck" "anybody up for breakfast burritos?"]
-                    [7 "daisy" "chuck: i love breakfast burritos!"]]}]})
+(def test-conversation-result nil)
 
-(deftest do-it-test
+(deftest main-test
   (is (= test-conversation-result
-         (do-it test-conversation))))
+         (-main test-conversation))))
