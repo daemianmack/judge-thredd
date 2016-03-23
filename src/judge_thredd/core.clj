3a9e0d12dac1d816f74bc6d8677e1a0d43534e2f
Daemian Mack <daemianmack@gmail.com>
2016-03-23 15:03:50 -0400

    Add docstrings to core ns.

--- src/judge_thredd/core.clj
+++ src/judge_thredd/core.clj
@@ -4,16 +4,22 @@
             [judge-thredd.chat-logs :refer [irc-log-messages]]))
 
 (defn wide-print
+  "Customize pprint to print (short) messages in easily-scanned manner."
   [messages]
   (binding [pp/*print-right-margin* 150]
     (pp/pprint messages)))
 
 (defn cosmetic-display
+  "Print messages to STDOUT."
   [messages]
   (prn :message-count (count messages))
   (prn :first-10-messages)
   (wide-print (take 10 messages)))
 
 (defn -main
+  "Goal: produce a data structure supporting a threaded display
+   as described in README.
+
+   Current status: merely prints IRC messages for your inspection."
   [filename]
   (cosmetic-display (irc-log-messages filename)))
