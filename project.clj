91f55c6e8cee23caada9a908efc77cfc52853a16
Daemian Mack <daemianmack@gmail.com>
2016-03-23 14:50:14 -0400

    Upgrade to Clojure 1.8 proper.

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "An exercise: consume an IRC log and attempt to produce a data structure that groups the disparate IRC messages by thread."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-time "0.10.0"]]
   :main judge-thredd.core)
