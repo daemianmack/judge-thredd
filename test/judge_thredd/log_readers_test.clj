833a20114d3f442162c23a0ba074b3ffdd12a7ee
Daemian Mack <daemianmack@gmail.com>
2023-07-12 09:25:34 -0400

    Unbreak test

--- test/judge_thredd/log_readers_test.clj
+++ test/judge_thredd/log_readers_test.clj
@@ -1,16 +1,16 @@
 (ns judge-thredd.log-readers-test
-  (:require [judge-thredd.log-readers :refer :all]
-            [clojure.test :refer :all]))
+  (:require [judge-thredd.log-readers :as log-readers]
+            [clojure.test :refer [deftest is testing]]))
 
 
 (def message-triples
   [[#inst "2015-08-07T08:45:00.000-00:00" "Alice" "Good morning!"]
    [#inst "2015-08-07T08:47:00.000-00:00" "Brian" "Alice: Good morning!"]
    [#inst "2015-08-07T08:52:00.000-00:00" "Chuck" "Anybody up for breakfast burritos?"]
-   [#inst "2015-08-07T08:53:00.000-00:00" "Daisy" "Chuck: I love breakfast burritos"]
+   [#inst "2015-08-07T08:53:00.000-00:00" "Daisy" "Chuck: I love those"]
    [#inst "2015-08-07T08:54:00.000-00:00" "Eddie" "Alice: morning!"]
    [#inst "2015-08-07T08:55:00.000-00:00" "Daisy" "Are they in the kitchen?"]
    [#inst "2015-08-07T08:56:00.000-00:00" "Chuck" "Daisy: yes"]])
 
 (deftest irc-messages-test
-  (is (= message-triples (irc-messages "example-irc-log-2015-08-07.txt"))))
+  (is (= message-triples (log-readers/irc-messages "example-irc-log-2015-08-07.txt"))))
